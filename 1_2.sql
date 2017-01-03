1a9826984f2649c16f2c70c9d17b38866f10351d
Marios Alexiou <it21405@hua.gr>
2017-01-03 19:40:30 +0200

    added 1_1 , 1_2

---
+++ 1_2.sql
@@ -0,0 +1,22 @@
+CREATE PROCEDURE fix_player_metrics AS
+  cm_height PLAYERS.HEIGHT%TYPE;
+  kg_weight PLAYERS.WEIGHT%TYPE;
+
+  CURSOR mCursor IS
+    SELECT
+      HEIGHT,
+      WEIGHT
+    FROM PLAYERS
+    FOR UPDATE;
+  BEGIN
+    OPEN mCursor;
+    LOOP
+      FETCH mCursor INTO cm_height, kg_weight;
+      EXIT WHEN mCursor%NOTFOUND;
+      cm_height := cm_height * 2.54;
+      kg_weight := kg_weight * 0.45;
+      UPDATE PLAYERS SET HEIGHT = cm_height , WEIGHT = kg_weight WHERE CURRENT OF mCursor;
+    END LOOP;
+    CLOSE mCursor;
+  END;
+/

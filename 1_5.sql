72e75d309166177b1d872ae0e038c85698140bf4
Evan Markou <viggie23bjj@gmail.com>
2017-01-04 17:39:15 +0200

    fixed 1_5

--- 1_5.sql
+++ 1_5.sql
@@ -1,15 +1,23 @@
 DROP FUNCTION calc_tendex;
 
-CREATE OR REPLACE FUNCTION calc_tendex(id IN VARCHAR2, year IN NUMBER, team IN VARCHAR2)
-  RETURN NUMBER IS
-  tendex NUMBER(5, 4);
+CREATE OR REPLACE FUNCTION calc_tendex(id IN VARCHAR2, nbaYear IN NUMBER, team IN VARCHAR2)
+  RETURN NUMBER AS
+  tendex Real;
   BEGIN
     SELECT (POINTS + REBOUNDS + ASSISTS + STEALS + BLOCKS - TURNOVERS - FOULS - (FGATTEMPTED - FGMADE)
             - (FTATTEMPTED - FTMADE) / 2) / MINUTES
     INTO tendex
     FROM PLAYERS_TEAMS pl
-    WHERE pl.PLAYERID = id AND pl.YEAR = year AND pl.TEAMID = team;
+    WHERE pl.PLAYERID = id AND pl.YEAR = nbaYear AND pl.TEAMID = team;
     RETURN tendex;
+    EXCEPTION
+    WHEN TOO_MANY_ROWS
+    THEN RETURN -1;
+    WHEN NO_DATA_FOUND
+    THEN RETURN 0;
+    WHEN OTHERS
+    THEN raise_application_error(
+      -20011, 'Unknown Exception in calc_tendex Function');
   END;
   /
 
@@ -17,14 +25,15 @@ ALTER TABLE PLAYERS_TEAMS
   ADD PRIMARY KEY (PLAYERID, TEAMID, YEAR);
 
 DECLARE
+
   id     VARCHAR2(26);
   year   NUMBER(6);
   team   VARCHAR2(4);
-  result REAL;
+  result NUMBER(6,4);
 
 BEGIN
   id := 'brezepr01';
-  year := '2009';
+  year := 2008;
   team := 'CHA';
   result := calc_tendex(id, year, team);
   dbms_output.put_line(' Player ' || id || ' in NBA Team ' || team || ' for the year ' || year || ' has ' || result);

694bd5e9dea28b080a29a2da7e0c264a14c86097
Georgios Alexandridis <galexandridis96@gmail.com>
2017-01-05 17:06:16 +0200

    fixed 2_2

--- 2_2.sql
+++ 2_2.sql
@@ -1,21 +1,41 @@
-SELECT XMLELEMENT("nba",
-                  XMLATTRIBUTES ('allstars' AS "dataset"),
-                  (SELECT XMLAGG(XMLELEMENT("division",
-                                            XMLATTRIBUTES (CONFERENCE AS "id"),
-                                            XMLAGG(XMLELEMENT("player",
-                                                              XMLFOREST(PLAYERID AS "id",
-                                                                        YEAR AS "year",
-                                                                        CONFERENCE AS "conference",
-                                                                        POINTS AS "points",
-                                                                        MINUTES AS "minutes"
-                                                              ))
+DROP TABLE TEMP_CLOB_TAB;
+CREATE TABLE TEMP_CLOB_TAB (
+  id  NUMBER,
+  xml XMLTYPE
+);
+CREATE OR REPLACE PROCEDURE get_allstar_players_xml AS
+  qryctx DBMS_XMLGEN.ctxhandle;
+  result XMLTYPE;
+  BEGIN
+    SELECT XMLELEMENT("nba",
+                      XMLATTRIBUTES ('allstars' AS "dataset"),
+                      (SELECT XMLAGG(XMLELEMENT("division",
+                                                XMLATTRIBUTES (CONFERENCE AS "id"),
+                                                XMLAGG(XMLELEMENT("player",
+                                                                  XMLFOREST(PLAYERID AS "id",
+                                                                            YEAR AS "year",
+                                                                            CONFERENCE AS "conference",
+                                                                            POINTS AS "points",
+                                                                            MINUTES AS "minutes"
+                                                                  ))
 
 
-                                            )
-                                 ))
-                   FROM ALLSTARS
-                   WHERE YEAR = 2009 AND CONFERENCE IN ('East', 'West')
-                   GROUP BY CONFERENCE
-                  )
-)
-FROM dual;
+                                                )
+                                     ))
+                       FROM ALLSTARS
+                       WHERE YEAR = 2009 AND CONFERENCE IN ('East', 'West')
+                       GROUP BY CONFERENCE)
+    )
+    INTO result
+    FROM dual;
+
+    INSERT INTO TEMP_CLOB_TAB VALUES (2,result);
+  END;
+/
+
+BEGIN
+  get_allstar_players_xml();
+END;
+/
+
+SELECT * FROM TEMP_CLOB_TAB;
